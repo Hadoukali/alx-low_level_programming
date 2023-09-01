cdc249305b798a3df22a0aa52ab1f9a906d56389
Hadoukali <hadoukali8@gmail.com>
2023-09-01 09:48:34 -0700

    REAME.md

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -9,18 +9,14 @@ char *_strpbrk(char *s, char *accept)
 {
 	int k;
 
-
 	while (*s)
 	{
 		for (k = 0; accept[k]; k++)
 		{
-		if (*s == accept[k])
-		return (s);
+			if (*s == accept[k])
+			return (s);
 		}
 	s++;
 	}
-
-
-return ('\0');
+	return ('\0');
 }
-
